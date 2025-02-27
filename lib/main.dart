ff6c87bc692935ed7ba9462f7bb329f17cdda727
nizarafham <nizarafham17@gmail.com>
2025-02-27 22:15:37 +0700

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Transaction Tracker',
+      title: 'Pocket Book!',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
